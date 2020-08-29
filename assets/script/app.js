e69eeddfb5471637d3ad6f693fca3d537a921f32
sambrosio17 <ambrosio.s@outlook.it>
2020-08-30 01:20:58 +0200

    v1.0 endend coding html, addedd some transition and js function to scroll to section

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,5 +1,37 @@
 $(document).ready(function () {
 
-    console.log("ok");
+
+
+    $(document).scroll(() => {
+
+        var y = $(this).scrollTop();
+        if (y > 100) {
+            $("#top").fadeIn();
+        } else {
+            $("#top").fadeOut();
+        }
+
+
+    });
+
+    $("#top").click(() => {
+
+        scroll({
+            top: 0,
+            behavior: "smooth"
+        });
+
+    });
 
 });
+
+
+const scrollToSection = (sectID) => {
+
+    var top = document.getElementById("#" + sectID).offsetTop;
+
+    scroll({
+        top: top,
+        behavior: "smooth"
+    });
+}
